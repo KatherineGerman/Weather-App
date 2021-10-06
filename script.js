f08f935199f61da9f5651b0d6c8103a58f8f7c8e
kathrd <katherine03gt@gmail.com>
2021-10-06 17:29:14 -0400

    Hide results while loading

--- script.js
+++ script.js
@@ -27,7 +27,11 @@ let weather = {
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed+" km/h";
-        
+        document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
     },
    search: function () {
       this.fetchWeather( document.querySelector(".search-bar").value);
@@ -38,4 +42,21 @@ let weather = {
 document.querySelector(".search button").addEventListener("click", function () {
 
 weather.search();
-})
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if(event.key == "Enter") {
+        weather.search();
+    }
+
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("Denver");
